ee206dc1b7f057e932680aa192ea793a51e871d6
Lev Kravinsky <kravinskylev@gmail.com>
2015-07-22 10:53:52 -0600

    clean up experiments

--- mock.rb
+++ mock.rb
@@ -1,33 +1,20 @@
-# class Calculator
-#   operators = {:+ => "add", :- => "subtract", :* => "multiply", :/ => "divide"}
-#
-#   operators.each do |sym, str|
-#     define_method("#{str}") do |*tuple|
-#       sym.to_proc.call(*tuple)
-#     end
-#   end
-# end
-
 class Mock
   def initialize(open_struct)
-    open_struct.each_pair do |method_name, return_value|  # => #<OpenStruct hi="123">
-      self.class.send(:define_method, method_name) do     # => Mock
-        return return_value                               # => "123"
-      end                                                 # => :hi
-    end                                                   # => {:hi=>"123"}
+    open_struct.each_pair do |method_name, return_value|
+      self.class.send(:define_method, method_name) do
+        return return_value
+      end
+    end
   end
 end
-os = OpenStruct.new(:hi => "123")                         # => #<OpenStruct hi="123">
-os.each_pair do |k,v|                                     # => #<OpenStruct hi="123">
-  "#{k}: #{v}"                                            # => "hi: 123"
-end                                                       # => {:hi=>"123"}
 
 class String
   def stub(method)
-    Mock.new(OpenStruct.new(method))  # => #<Mock:0x007fc663923700>
+    Mock.new(OpenStruct.new(method))
   end
 end
-jeff = "hi"                           # => "hi"
-mock = jeff.stub(:hi => "123")        # => #<Mock:0x007fc663923700>
-mock.methods - Object.methods         # => [:hi]
-mock.hi                               # => "123"
+
+jeff = "hi"
+mock = jeff.stub(:hi => "123")
+mock.methods - Object.methods
+mock.hi
