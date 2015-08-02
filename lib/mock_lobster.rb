7a988b4eb4a0a0797f1f729cd34521945499e462
Lev Kravinsky <kravinskylev@gmail.com>
2015-08-02 14:27:18 -0600

    comment out broken expectation

--- lib/mock_lobster.rb
+++ lib/mock_lobster.rb
@@ -2,7 +2,7 @@ require 'ostruct'
 
 class Object
   def mock(method)
-    Mock.new(OpenStruct.new(method))
+    Mock.new(method)
   end
 
   def stubbify(hash)
@@ -28,30 +28,30 @@ class Object
   end
 
 
-  def expect(method, param)
-    Expectation.new(method, param, self)
-  end
+#  def expect(method, param)
+#    Expectation.new(method, param, self)
+#  end
 end
 
 class Mock
-  def initialize(open_struct)
-    open_struct.each_pair do |method_name, return_value|
+  def initialize(method)
+    method.each do |method_name, return_value|
       self.send(:define_singleton_method, method_name) do
-        return return_value
+        return_value
       end
     end
   end
 end
 
-class Expectation
-  attr_accessor :method, :self, :param   # => nil
-  def initialize(method, param, object)
-    @method = method
-    @object = object
-  end
-
-  def returns(result)
-    @object.send(:define_singleton_method, method) { |param| result }
-    result
-  end
-end
+#class Expectation
+#  attr_accessor :method, :self, :param   # => nil
+#  def initialize(method, param, object)
+#    @method = method
+#    @object = object
+#  end
+#
+#  def returns(result)
+#    @object.send(:define_singleton_method, method) { |param| result }
+#    result
+#  end
+#end
